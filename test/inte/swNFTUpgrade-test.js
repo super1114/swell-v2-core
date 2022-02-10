d2e4776948e0b65387a35d5af03375aff54b8149
Lecky <leckylao@gmail.com>
2022-02-10 21:48:39 +1100

    fixing CI test

--- test/inte/swNFTUpgrade-test.js
+++ test/inte/swNFTUpgrade-test.js
@@ -17,12 +17,7 @@ describe("SWNFTUpgrade", async () => {
 
   before(async () => {
     [signer] = await ethers.getSigners();
-    const Helpers = await ethers.getContractFactory("Helpers");
-    const helpers = await Helpers.deploy();
     const SWNFTUpgrade = await ethers.getContractFactory("SWNFTUpgrade");
-    // const SWNFTUpgrade = await ethers.getContractFactory("SWNFTUpgrade", { libraries: {
-    //   Helpers: helpers.address,
-    // }});
     swNFTUpgrade = await upgrades.deployProxy(SWNFTUpgrade, ["Swell NFT", "swNFT"], {kind: "uups"});
     await swNFTUpgrade.deployed();
     console.log("swNFTUpgrade deployed to:", swNFTUpgrade.address);
@@ -57,34 +52,6 @@ describe("SWNFTUpgrade", async () => {
   });
 
   it("can stake more than 1 ether", async function() {
-    // let logStateEvent = new Promise<any>((resolve, reject) => {
-    //   swNFT.on(
-    //     "LogStake",
-    //     (
-    //       user,
-    //       itemId,
-    //       validatorIndex,
-    //       deposit,
-    //       event,
-    //     ) => {
-    //       event.removeListener();
-
-    //       resolve({
-    //         user: user,
-    //         itemId: itemId,
-    //         validatorIndex: validatorIndex,
-    //         deposit: deposit,
-    //       });
-    //     },
-    //   );
-
-    //   setTimeout(() => {
-    //     reject(new Error("timeout"));
-    //   }, 60000);
-    // });
-
-    // let event = await logStateEvent;
-
     await swNFTUpgrade.stake(
       pubkey,
       withdrawal_credentials,
@@ -94,25 +61,11 @@ describe("SWNFTUpgrade", async () => {
       { value: ethers.utils.parseEther("1") }
     );
 
-    // expect(event.user).to.equal(signer.address);
-    // expect(event.itemId).to.equal("1");
-    // expect(event.validatorIndex).to.equal("28899");
-    // expect(event.deposit).to.equal(ethers.utils.parseEther("1"));
-
     const tokenURI = await swNFTUpgrade.tokenURI("1");
-    // expect(tokenURI).to.be.equal(
-    //   "0xa5e7f4a06080b860d376871ce0798aa7677e7a4b117a5bd0909f15fee02f28a62388496982c133fef1eba087d8a06005/1000000000000000000.json"
-    // );
     expect(tokenURI).to.be.equal(
       "https://raw.githubusercontent.com/leckylao/Eth2S/main/metaData/0xa5e7f4a06080b860d376871ce0798aa7677e7a4b117a5bd0909f15fee02f28a62388496982c133fef1eba087d8a06005/1000000000000000000.json"
     );
 
-    // const token = await swNFTUpgrade.tokens("1");
-    // expect(token.pubKey).to.be.equal(
-    //   "0xa5e7f4a06080b860d376871ce0798aa7677e7a4b117a5bd0909f15fee02f28a62388496982c133fef1eba087d8a06005"
-    // );
-    // expect(token.validatorIndex).to.be.equal("28899");
-    // expect(token.deposit).to.equal(ethers.utils.parseEther("1"));
   });
 
   it("cannot stake more than 32 ether", async function() {
@@ -128,14 +81,4 @@ describe("SWNFTUpgrade", async () => {
     ).to.be.revertedWith("cannot stake more than 32 ETH");
   });
 
-  // it("Should be able to unstake when ETH2 phrase 1", async function() {
-  //   const tx = await eTH2Staking.unstake(ether("1"), {from: sender});
-  //   expectEvent(tx, "Transfer", {
-  //     from: sender,
-  //     to: constants.ZERO_ADDRESS,
-  //     value: ether("1")
-  //   })
-  //   const balance = await eTH2Staking.balanceOf(sender);
-  //   expect(balance).to.be.bignumber.equal("0");
-  // });
 });
