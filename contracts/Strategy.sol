eec16895309339322851e7370b87e16fdf95e7a1
Lecky <leckylao@gmail.com>
2022-02-10 21:28:10 +1100

    feat(swNFT, Strategy): Adding dummy strategy

---
+++ contracts/Strategy.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.9;
+
+import "./interfaces/IStrategy.sol";
+import "./interfaces/ISWETH.sol";
+import "./interfaces/ISWNFT.sol";
+
+/// @title Contract for Strategy
+contract Strategy is IStrategy {
+    /// @dev The token ID position data
+    mapping(uint256 => uint) public positions;
+
+    address public swNFT;
+
+    constructor(address _swNFT) {
+        swNFT = _swNFT;
+    }
+
+    modifier onlyswNFT {
+        require(msg.sender == swNFT);
+        _;
+    }
+
+    function enter(uint tokenId, uint amount) external onlyswNFT returns (bool success) {
+        address baseTokenAddress = ISWNFT(swNFT).baseTokenAddress();
+        success = ISWETH(baseTokenAddress).transferFrom(msg.sender, address(this), amount);
+        if(!success) return success;
+        positions[tokenId] += amount;
+        emit LogEnter(tokenId, amount);
+    }
+
+    function exit(uint tokenId) external onlyswNFT returns (uint amount) {
+        address baseTokenAddress = ISWNFT(swNFT).baseTokenAddress();
+        amount = positions[tokenId];
+        require(amount > 0, "No position to exit");
+        bool success = ISWETH(baseTokenAddress).transfer(msg.sender, amount);
+        if(!success) return 0;
+        positions[tokenId] -= amount;
+        emit LogExit(tokenId, amount);
+    }
+}
---
+++ contracts/interfaces/IStrategy.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.9;
+
+/// @title Interface for swETH
+interface IStrategy {
+    function enter(uint tokenId, uint amount) external returns (bool success);
+    function exit(uint tokenId) external returns (uint amount);
+
+    // ============ Events ============
+
+    event LogEnter(
+        uint tokenId,
+        uint amount
+    );
+
+    event LogExit(
+        uint tokenId,
+        uint amount
+    );
+}
