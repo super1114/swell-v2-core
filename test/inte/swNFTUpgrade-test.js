e8e42662b92d32c4e911138870d6fcd1a15e6962
Kevin Christopher <97299783+KevinChristopher1014@users.noreply.github.com>
2022-04-05 20:21:44 -0400

    Update swNFTUpgrade-test.js

--- test/inte/swNFTUpgrade-test.js
+++ test/inte/swNFTUpgrade-test.js
@@ -74,8 +74,8 @@ describe("SWNFTUpgrade", async () => {
 
   it("can update OpRate", async function() {
     await swNFT.connect(user).updateOpRate("10");
-    const opRate = await swNFT.opRate(user);
-    // expect(opRate).to.be.equal("10");
+    const opRate = await swNFT.opRate(user.address);
+    expect(opRate).to.be.equal("10");
   });
 
   it("can stake 1 Ether", async function() {
