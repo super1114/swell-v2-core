9325eb6e48000c69c507975a005d7c90364cea0c
Kevin Christopher <97299783+KevinChristopher1014@users.noreply.github.com>
2022-04-06 22:29:13 -0400

    feat(opRate) : Need to add mapping of address to uint to store node operator rate for the frontend.

--- test/unit/swNFT-test.js
+++ test/unit/swNFT-test.js
@@ -1,15 +1,23 @@
-// import { BigNumber, constants, Wallet } from 'ethers'
-// import { waffle, ethers } from 'hardhat'
 const { ethers, upgrades } = require("hardhat");
 const { expect } = require("chai");
-// import { Fixture } from 'ethereum-waffle'
 
-describe("SWNFT", () => {
+describe("SWNFT", async () => {
+  const pubKey =
+    "0xb57e2062d1512a64831462228453975326b65c7008faaf283d5e621e58725e13d10f87e0877e8325c2b1fe754f16b1ec";
+  const signature =
+    "0xb224d558d829c245fe56bff9d28c7fd0d348d6795eb8faef8ce220c3657e373f8dc0a0c8512be589ecaa749fe39fc0371380a97aab966606ba7fa89c78dc1703858dfc5d3288880a813e7743f1ff379192e1f6b01a6a4a3affee1d50e5b3c849";
+  const depositDataRoot =
+    "0x81a814655bfc695f5f207d433b4d2e272d764857fee6efd58ba4677c076e60a9";
   const depositAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
-  let swNFT, swETH, signer, user;
+  const zeroAddress = "0x0000000000000000000000000000000000000000";
+  let swNFT, swETH, signer, user, strategy, swDAO;
 
   before(async () => {
     [signer, user] = await ethers.getSigners();
+
+    const SWDAO = await ethers.getContractFactory("SWDAO");
+    swDAO = await SWDAO.deploy();
+    await swDAO.deployed();
 
     // const SWNFTUpgrade = await ethers.getContractFactory("SWNFTUpgrade");
     const nftDescriptorLibraryFactory = await ethers.getContractFactory(
@@ -36,6 +44,10 @@ describe("SWNFT", () => {
     swETH = await SWETH.deploy(swNFT.address);
     await swETH.deployed();
     await swNFT.setswETHAddress(swETH.address);
+
+    const Strategy = await ethers.getContractFactory("Strategy");
+    strategy = await Strategy.deploy(swNFT.address);
+    await strategy.deployed();
   });
 
   it("can update OpRate", async function() {
