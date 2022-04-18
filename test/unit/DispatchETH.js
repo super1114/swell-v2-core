ffb6c117f1c890fb10b171333d536d34de1a2a8d
super1114 <super1114dev@gmail.com>
2022-04-18 11:00:56 +0200

    updated Unit test

--- test/unit/DispatchETH.js
+++ test/unit/DispatchETH.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Dispatch Test ETH", async () => {
+describe("Dispatch Test ETH", () => {
   let signer, user1, MultiSender, multisender;
   beforeEach(async () => {
     [signer, user1] = await ethers.getSigners();
@@ -12,8 +12,8 @@ describe("Dispatch Test ETH", async () => {
   it("Should transfer ETH", async () => {
     const val = ethers.utils.parseEther("10");
     const data = [user1.address];
-    // expect(multisender.connect(signer).multiSend(data, { value: val }))
-    //   .to.emit(multisender, "MultiSend")
-    //   .withArgs(signer.getAddress(), val);
+    expect(multisender.connect(signer).multiSend(data, { value: val }))
+      .to.emit(multisender, "MultiSend")
+      .withArgs(signer.address, val);
   });
 });
