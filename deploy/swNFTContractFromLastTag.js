6b3103a01271d9c31a711ce646896e1a5fb4c9b1
smart-ace <smart.solidity@gmail.com>
2022-06-10 18:58:27 -0400

    fix: always start from root directory

--- deploy/swNFTContractFromLastTag.js
+++ deploy/swNFTContractFromLastTag.js
@@ -5,15 +5,15 @@ const execProm = util.promisify(exec);
 
 const getLastTagContractFactory = async () => {
   const tag = await getTag();
+  console.log({ tag });
   await execProm(
     "git clone git@github.com:SwellNetwork/v2-core.git contracts/latest"
   );
-  await execProm("cd contracts/latest");
   await execProm(
-    `git checkout tags/${tag} -b automatic-latest-testing-${new Date().getTime()}`
+    `cd contracts/latest && git checkout tags/${tag} -b automatic-latest-testing-${new Date().getTime()}`
   );
-  await execProm("mv contracts ../latest-tag");
-  await execProm("cd .. && rm -rf latest");
+  await execProm("mv contracts/latest/contracts contracts/latest-tag");
+  await execProm("rm -rf contracts/latest");
   await execProm(`npx hardhat compile`);
   console.log("--> old contract factory getting done");
 };
