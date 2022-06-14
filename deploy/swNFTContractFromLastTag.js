6b7c975496d2a611ba089029d0dcb891a1c6da1a
smart-ace <smart.solidity@gmail.com>
2022-06-14 19:55:56 -0400

    fix: CI/CD to latest tag

--- deploy/swNFTContractFromLastTag.js
+++ deploy/swNFTContractFromLastTag.js
@@ -7,7 +7,6 @@ const getLastTagContractFactory = async () => {
   const tag = await getTag();
   console.log({ tag });
   await execProm("rm -rf contracts/latest-tag");
-  console.log("env", process.env.SKIP_GIT_CLONE);
   if (process.env.SKIP_GIT_CLONE === "false") {
     await execProm("git clone git@github.com:SwellNetwork/v2-core.git latest");
     await execProm(
