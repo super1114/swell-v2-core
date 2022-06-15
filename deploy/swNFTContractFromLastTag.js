737ad4f80e7f3b1b850d39e1fb8c7445fd2df9ea
smart-ace <smart.solidity@gmail.com>
2022-06-14 20:19:05 -0400

    fix: CI/CD for unit testing

--- deploy/swNFTContractFromLastTag.js
+++ deploy/swNFTContractFromLastTag.js
@@ -6,16 +6,16 @@ const execProm = util.promisify(exec);
 const getLastTagContractFactory = async () => {
   const tag = await getTag();
   console.log({ tag });
+  await execProm("rm -rf contracts/latest-tag");
   if (process.env.SKIP_GIT_CLONE === "false") {
-    await execProm("rm -rf contracts/latest-tag");
     await execProm("git clone git@github.com:SwellNetwork/v2-core.git latest");
     await execProm(
       `cd latest && git checkout tags/${tag} -b automatic-latest-testing-${new Date().getTime()}`
     );
-    await execProm("mv latest/contracts contracts/latest-tag");
-    await execProm("rm -rf latest");
-    await execProm(`npx hardhat compile`);
   }
+  await execProm("mv latest/contracts contracts/latest-tag");
+  await execProm("rm -rf latest");
+  await execProm(`npx hardhat compile`);
   console.log("--> old contract factory getting done");
 };
 
