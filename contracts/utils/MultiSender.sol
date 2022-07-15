290b2c99d65ad0a18c46f60ff214b5c5c8a857b3
Aaron Alderman <hello@aaronalderman.com>
2022-07-15 13:43:49 +0800

    Use send instead of transfer - must do something with return value otherwise there are warnings

--- contracts/utils/MultiSender.sol
+++ contracts/utils/MultiSender.sol
@@ -5,13 +5,15 @@ contract MultiSender {
     constructor() {}
 
     event MultiSend(address indexed sender, uint256 value);
+    event MultiSendFailure(address indexed recieveFailed);
 
     function multiSend(address[] calldata addrs) external payable {
         require(msg.value > 0, "No value");
         require(addrs.length > 0);
         uint256 dispatchAmount = msg.value / addrs.length;
         for (uint256 i = 0; i < addrs.length; i++) {
-            payable(addrs[i]).transfer(dispatchAmount);
+            bool wasSent = payable(addrs[i]).send(dispatchAmount);
+            if (!wasSent) emit MultiSendFailure(addrs[i]);
         }
         emit MultiSend(msg.sender, msg.value);
     }
